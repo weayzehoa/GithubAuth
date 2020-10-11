9c82209afae44d8dcc108c31a3e74f6402a79730
Roger Wu <weayzehoa@gmail.com>
2020-10-11 13:36:30 +0800

    安裝 Laravel Socialite 套件並設定相關資料

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
     composer require laravel/ui "^1.0" --dev
     php artisan ui vue --auth
     npm install && npm run dev
+
+2. 安裝 Laravel Socialite 套件並設定相關資料
+    composer require laravel/socialite
--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laravel\Socialite\SocialiteServiceProvider::class,
     ],
 
     /*
@@ -226,6 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ],
 
 ];
